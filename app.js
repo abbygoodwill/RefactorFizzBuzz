83ae5d77a24c0b5b3c0cce0735351bfcf18d0959
abbygoodwill <abby.goodwill@gmail.com>
2016-01-20 11:15:03 -0700

    second commit

---
+++ app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	var endNumber = prompt("Enter a number:");
+	console.log(typeof(endNumber));
+	console.log(typeof(endNumber));
+	isInteger(endNumber);
+});
+
+function isInteger(input) {
+        if(input % 1 === 0) {
+        	return lister(input);
+    	} else {
+    		var secondNumber = prompt("Please only enter numbers in: ");
+    		return lister(secondNumber);
+    	}
+    }
+function lister(number) {
+	for (var i = 1; i <= number; i++) {
+		if ((i % 3 == 0) && (i % 5 == 0)) {
+			$('#fizzbuzzPrint').append('<li class="toadd">FizzBuzz</li>');
+		} else if (i % 3 == 0) {
+					$('#fizzbuzzPrint').append('<li class="toadd">Fizz</li>');
+		} else if (i % 5 == 0) {
+			$('#fizzbuzzPrint').append('<li class="toadd">Buzz</li>');
+		} else {
+			$('#fizzbuzzPrint').append('<li class="toadd">' + i + '</li>');
+		}
+	}
+}
